b365735f315a9c3f6aa43581588303db3eb5673f
Fabricio Caneva <hardlymersk@gmail.com>
2020-05-28 14:42:47 -0300

    add setup and teardown callbacks

--- lib/flutter_phoenix.dart
+++ lib/flutter_phoenix.dart
@@ -5,8 +5,10 @@ import 'package:flutter/widgets.dart';
 /// Wrap your root App widget in this widget and call [Phoenix.rebirth] to restart your app.
 class Phoenix extends StatefulWidget {
   final Widget child;
+  final VoidCallback setup;
+  final VoidCallback tearDown;
 
-  Phoenix({this.child});
+  Phoenix({@required this.child, this.setup, this.tearDown});
 
   @override
   _PhoenixState createState() => _PhoenixState();
@@ -19,10 +21,21 @@ class Phoenix extends StatefulWidget {
 class _PhoenixState extends State<Phoenix> {
   Key _key = UniqueKey();
 
+  @override
+  void initState() {
+    super.initState();
+
+    widget?.setup();
+  }
+
   void restartApp() {
+    widget?.tearDown();
+
     setState(() {
       _key = UniqueKey();
     });
+
+    widget?.setup();
   }
 
   @override
